0d8720293b6d82336f5e4c7ab9b5c12509f9c068
venkatlaxmareddy <venkatlaxmareddyyenna@gmail.com>
2025-10-14 21:29:42 -0700

    CI/CD test - updated server message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from project0 Docker container! Updated at ' + new Date().toLocaleTimeString() + '\n');
+  res.end('this is testing for CI/CD Updated at ' + new Date().toLocaleTimeString() + '\n');
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
