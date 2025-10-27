82deb56ab097e7a9136e98dd4f82f3ba42662fb3
venkatlaxmareddy <venkatlaxmareddyyenna@gmail.com>
2025-10-26 17:31:56 -0700

    Test CI/CD - Updated message

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('this is testing for CI/CD Updated at ' + new Date().toLocaleTimeString() + '\n');
+  res.end('Hello from Project5! CI/CD pipeline is working perfectly at ' + new Date().toLocaleTimeString() + '\n');
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
